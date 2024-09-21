d62d88965427819d55c19ce4e13af8d1abf416d4
Андрей Крапивин <without.a.body@gmail.com>
2024-09-21 15:31:55 +0300

    HW 30. Company implementation

---
+++ src/main/java/telran/employees/CompanyImpl.java
@@ -0,0 +1,130 @@
+package telran.employees;
+
+import java.util.*;
+
+public class CompanyImpl implements Company {
+    private TreeMap<Long, Employee> employees = new TreeMap<>();
+    private HashMap<String, List<Employee>> employeesDepartment = new HashMap<>();
+    private TreeMap<Float, List<Manager>> managersFactor = new TreeMap<>();
+
+    private class CompanyImplIterator implements Iterator<Employee> {
+        private Iterator<Map.Entry<Long, Employee>> iterator;
+        private Map.Entry<Long, Employee> currentEntry = null;
+
+        public CompanyImplIterator() {
+            this.iterator = employees.entrySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Employee next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            currentEntry = iterator.next();
+            return currentEntry.getValue();
+        }
+
+        @Override
+        public void remove() {
+            if (currentEntry == null) {
+                throw new IllegalStateException();    
+            }
+            iterator.remove();
+            removeEmployeeInOtherCollection(currentEntry.getValue());
+            currentEntry = null;
+        }
+    }
+
+    @Override
+    public Iterator<Employee> iterator() {
+        return new CompanyImplIterator();
+    }
+
+    @Override
+    public void addEmployee(Employee empl) {
+        if (employees.putIfAbsent(empl.getId(), empl) == null) {
+            employeesDepartment.computeIfAbsent(empl.getDepartment(), k -> new ArrayList<>()).add(empl);
+            if (empl instanceof Manager) {
+                Manager manager = (Manager) empl;
+                managersFactor.computeIfAbsent(manager.getFactor(), k -> new ArrayList<>()).add(manager);
+            }
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    @Override
+    public Employee getEmployee(long id) {
+        return employees.get(id);
+    }
+
+    @Override
+    public Employee removeEmployee(long id) {
+        Employee empl = employees.remove(id);
+        removeEmployeeInOtherCollection(empl);
+        return empl;
+    }
+
+    private void removeEmployeeInOtherCollection(Employee empl) {
+        if (empl != null) {
+            removeFromEmployeesDepartment(empl);
+            if (empl instanceof Manager) {
+                removeFromManagersFactor((Manager)empl);
+            }
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    private void removeFromManagersFactor(Manager empl) {
+        float factor = empl.getFactor();
+        List<Manager> list = managersFactor.get(factor);
+        if (list != null) {
+            list.remove(empl);
+            if (list.isEmpty()) {
+                managersFactor.remove(factor);
+            }
+        }
+    }
+
+    private void removeFromEmployeesDepartment(Employee empl) {
+        String department = empl.getDepartment();
+        List<Employee> list = employeesDepartment.get(department);
+        if (list != null) {
+            list.remove(empl);
+            if (list.isEmpty()) {
+                employeesDepartment.remove(department);
+            }
+        }  
+    }
+
+    @Override
+    public int getDepartmentBudget(String department) {
+        List<Employee> list = employeesDepartment.get(department);
+        int res = 0;
+        if (list != null) {
+            res = list.stream().mapToInt(Employee::computeSalary).sum();    
+        }
+         return res;   
+    }
+
+    @Override
+    public String[] getDepartments() {
+        return employeesDepartment.keySet().stream().sorted().toArray(String[]::new);
+    }
+
+    @Override
+    public Manager[] getManagersWithMostFactor() {
+        Manager[] res = new Manager[0];
+        if (!managersFactor.isEmpty()) {
+            res = managersFactor.lastEntry().getValue().toArray(Manager[]::new);
+        }
+        return res;
+    }
+
+}
